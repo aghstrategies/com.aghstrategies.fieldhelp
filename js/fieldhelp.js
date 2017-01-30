f8b2d2d799ee23944e8494651eb7fb93fc273497
Alice Frumin <alice@aghstrategies.com>
2017-01-30 17:02:39 -0500

    AGH #7791 wrapping js in a function and calling on crmLoad

--- js/fieldhelp.js
+++ js/fieldhelp.js
@@ -1,22 +1,29 @@
 CRM.$(function ($) {
-  var $fields = CRM.vars.fieldhelp.fields;
-  console.log($fields);
-  $.each($fields, function (key, value) {
-    var $fieldInput = $('#' + key);
-    if (key == 'Email_Block_1') {
-      console.log($fieldInput);
-    }
+  var addFieldHelp = function () {
+    var $fields = CRM.vars.fieldhelp.fields;
+    console.log($fields);
+    $.each($fields, function (key, value) {
+      var $fieldInput = $('#' + key);
+      if (key == 'Email_Block_1') {
+        console.log($fieldInput);
+      }
 
-    if (key == 'preferred_communication_method_1') {
-      $('<div/>', {
-        class: 'description',
-        text: value,
-      }).insertBefore($fieldInput);
-    } else {
-      $('<div/>', {
-        class: 'description',
-        text: value,
-      }).insertAfter($fieldInput);
-    }
+      if (key == 'preferred_communication_method_1') {
+        $('<div/>', {
+          class: 'description',
+          text: value,
+        }).insertBefore($fieldInput);
+      } else {
+        $('<div/>', {
+          class: 'description',
+          text: value,
+        }).insertAfter($fieldInput);
+      }
+    });
+  };
+
+  addFieldHelp();
+  $(document).on('crmLoad', function () {
+    addFieldHelp();
   });
 });
