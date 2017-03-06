618ed284518098995abe4cc641ecaf55e356eab5
AGH Strategies <support@aghstrategies.com>
2017-03-06 13:55:44 -0500

    AGH #7791 dealing with field text showing up twice on cases

--- js/fieldhelp.js
+++ js/fieldhelp.js
@@ -1,19 +1,22 @@
 CRM.$(function ($) {
   var addFieldHelp = function () {
     var $fields = CRM.vars.fieldhelp.fields;
-    console.log($fields);
     $.each($fields, function (key, value) {
       var $fieldInput = $('#' + key);
-      if (key == 'preferred_communication_method_1') {
-        $('<div/>', {
-          class: 'description',
-          text: value,
-        }).insertBefore($fieldInput);
-      } else {
-        $('<div/>', {
-          class: 'description',
-          text: value,
-        }).insertAfter($fieldInput);
+      if ($('#' + key + 'descrip').length == 0) {
+        if (key == 'preferred_communication_method_1') {
+          $('<div/>', {
+            class: 'description',
+            id: key + 'descrip',
+            text: value,
+          }).insertBefore($fieldInput);
+        } else {
+          $('<div/>', {
+            class: 'description',
+            id: key + 'descrip',
+            text: value,
+          }).insertAfter($fieldInput);
+        }
       }
     });
   };
